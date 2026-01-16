a3cb34a35a57635e7f5428493336abaecddfa879
Sylvain MOCARD <sylvain.mocard@iot.bzh>
2026-01-16 16:56:59 +0100

    feat: Migrate Stylelint to `stylelint-config-standard-scss`, add related packages, and adjust ESLint rules.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-    "extends": "stylelint-config-standard",
-    "plugins": ["stylelint-scss"],
-    "rules": {
-      "at-rule-no-unknown": null,
-      "scss/at-rule-no-unknown": null,
-      "no-descending-specificity": null,
-      "selector-type-no-unknown" : null,
-      "selector-pseudo-element-no-unknown" : null,
-      "value-list-max-empty-lines" : 1,
-      "function-max-empty-lines" : 1,
-    },
-  }
+  "extends": "stylelint-config-standard-scss",
+  "rules": {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": null,
+    "no-descending-specificity": null,
+    "selector-type-no-unknown": null,
+    "selector-pseudo-element-no-unknown": null,
+    "scss/function-no-unknown": [true, {
+      "ignoreFunctions": ["nb-register-theme", "nb-theme", "nb-install-component"]
+    }],
+    "import-notation": null
+  },
+}
